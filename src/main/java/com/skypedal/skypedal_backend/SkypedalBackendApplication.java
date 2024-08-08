b7486386d131fc00ad84cfe4ecf9ba2b7aee7ea7
William Moolman <moolman.william@gmail.com>
2024-08-08 16:33:15 +0100

    initial commit

---
+++ src/main/java/com/skypedal/skypedal_backend/SkypedalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skypedal.skypedal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkypedalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkypedalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypedal/skypedal_backend/SkypedalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypedal.skypedal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypedalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
